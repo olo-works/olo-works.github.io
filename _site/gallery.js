1156c89a453232aaa01e208125b54bf534cf787c
olo-works <nicolo.simons@gmail.com>
2026-03-21 19:09:45 +0100

    0.4

--- _site/gallery.js
+++ _site/gallery.js
@@ -1,29 +1,17 @@
 const allImages = [
   
     
-      { src: "/images/projects/ahok/cover.png", title: "ah ok Collection", tag: "Product", href: "/projects/ahok/" },
+      { src: "/images/projects/ahok/cover.png", title: "ah ok Collection", tag: "Product", href: "/projects/ah-ok/" },
     
     
       
-        { src: "/images/projects/ahok/render-full.png", title: "ah ok Collection", tag: "Product", href: "/projects/ahok/" },
+        { src: "/images/projects/ahok/render-full.png", title: "ah ok Collection", tag: "Product", href: "/projects/ah-ok/" },
       
-        { src: "/images/projects/ahok/sedia.png", title: "ah ok Collection", tag: "Product", href: "/projects/ahok/" },
+        { src: "/images/projects/ahok/context-day.png", title: "ah ok Collection", tag: "Product", href: "/projects/ah-ok/" },
       
-        { src: "/images/projects/ahok/tavolino.png", title: "ah ok Collection", tag: "Product", href: "/projects/ahok/" },
+        { src: "/images/projects/ahok/context-night.png", title: "ah ok Collection", tag: "Product", href: "/projects/ah-ok/" },
       
-        { src: "/images/projects/ahok/lampada.png", title: "ah ok Collection", tag: "Product", href: "/projects/ahok/" },
-      
-        { src: "/images/projects/ahok/appendiabiti.png", title: "ah ok Collection", tag: "Product", href: "/projects/ahok/" },
-      
-        { src: "/images/projects/ahok/context-day.png", title: "ah ok Collection", tag: "Product", href: "/projects/ahok/" },
-      
-        { src: "/images/projects/ahok/context-night.png", title: "ah ok Collection", tag: "Product", href: "/projects/ahok/" },
-      
-        { src: "/images/projects/ahok/detail-lamp.png", title: "ah ok Collection", tag: "Product", href: "/projects/ahok/" },
-      
-        { src: "/images/projects/ahok/detail-coat.png", title: "ah ok Collection", tag: "Product", href: "/projects/ahok/" },
-      
-        { src: "/images/projects/ahok/tecnici.png", title: "ah ok Collection", tag: "Product", href: "/projects/ahok/" },
+        { src: "/images/projects/ahok/tecnici.png", title: "ah ok Collection", tag: "Product", href: "/projects/ah-ok/" },
       
     
   
@@ -52,52 +40,36 @@ const allImages = [
     
   
     
-      { src: "/images/projects/memoe/cover.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
+      { src: "/images/projects/memoe/cover.png", title: "Me-Moe", tag: "Product", href: "/projects/me-moe/" },
     
     
       
-        { src: "/images/projects/memoe/render-street.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
+        { src: "/images/projects/memoe/render-street.png", title: "Me-Moe", tag: "Product", href: "/projects/me-moe/" },
       
-        { src: "/images/projects/memoe/render-front.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
+        { src: "/images/projects/memoe/render-front.png", title: "Me-Moe", tag: "Product", href: "/projects/me-moe/" },
       
-        { src: "/images/projects/memoe/render-rear-open.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
+        { src: "/images/projects/memoe/render-rear-open.png", title: "Me-Moe", tag: "Product", href: "/projects/me-moe/" },
       
-        { src: "/images/projects/memoe/render-night.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
+        { src: "/images/projects/memoe/render-night.png", title: "Me-Moe", tag: "Product", href: "/projects/me-moe/" },
       
-        { src: "/images/projects/memoe/render-colors.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
+        { src: "/images/projects/memoe/render-colors.png", title: "Me-Moe", tag: "Product", href: "/projects/me-moe/" },
       
-        { src: "/images/projects/memoe/vano-cobot.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
-      
-        { src: "/images/projects/memoe/struttura-exploded.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
-      
-        { src: "/images/projects/memoe/riadattabilita-modules.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
-      
-        { src: "/images/projects/memoe/riadattabilita-retrofit.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
-      
-        { src: "/images/projects/memoe/agadexo-render.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
-      
-        { src: "/images/projects/memoe/agadexo-deploy.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
-      
-        { src: "/images/projects/memoe/pacchi-full.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
-      
-        { src: "/images/projects/memoe/app-screens.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
-      
-        { src: "/images/projects/memoe/tecnici.png", title: "Me-Moe", tag: "Product", href: "/projects/memoe/" },
+        { src: "/images/projects/memoe/tecnici.png", title: "Me-Moe", tag: "Product", href: "/projects/me-moe/" },
       
     
   
     
-      { src: "/images/photoprojects/logosquare.svg", title: "My Personal Identity", tag: "Visual", href: "/projects/olo-works-identity/" },
+      { src: "/images/photoprojects/logosquare.svg", title: "My Personal Identity", tag: "Visual", href: "/projects/personal-identity/" },
     
     
       
-        { src: "/images/photoprojects/logo-primary.png", title: "My Personal Identity", tag: "Visual", href: "/projects/olo-works-identity/" },
+        { src: "/images/photoprojects/logo-primary.png", title: "My Personal Identity", tag: "Visual", href: "/projects/personal-identity/" },
       
-        { src: "/images/photoprojects/color-system.png", title: "My Personal Identity", tag: "Visual", href: "/projects/olo-works-identity/" },
+        { src: "/images/photoprojects/color-system.png", title: "My Personal Identity", tag: "Visual", href: "/projects/personal-identity/" },
       
-        { src: "/images/photoprojects/type-specimen.png", title: "My Personal Identity", tag: "Visual", href: "/projects/olo-works-identity/" },
+        { src: "/images/photoprojects/type-specimen.png", title: "My Personal Identity", tag: "Visual", href: "/projects/personal-identity/" },
       
-        { src: "/images/photoprojects/stationery.png", title: "My Personal Identity", tag: "Visual", href: "/projects/olo-works-identity/" },
+        { src: "/images/photoprojects/stationery.png", title: "My Personal Identity", tag: "Visual", href: "/projects/personal-identity/" },
       
     
   
